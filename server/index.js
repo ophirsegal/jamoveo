cd94017f71d80773a0ccb93509d7f81add2e3c85
ophir segal <ophirsegal@ophirsMacBook.local>
2025-06-08 11:17:32 +0300

    test

--- server/index.js
+++ server/index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const http = require("http");
-const cors = require("cors");
-const socketIo = require("socket.io"); 
+// const cors = require("cors");
+// const socketIo = require("socket.io"); 
 const mongoose = require("mongoose");
-require("dotenv").config(); 
+require("dotenv").config();
+// require("./db")(); 
 
 const app = express();
 const server = http.createServer(app);
 
-const FRONTEND_URL = process.env.NODE_ENV === 'production'
-  ? "https://jamoveo-afqmjx21u-ophir-segals-projects.vercel.app"
-  : "http://localhost:3000";
 
-app.use(cors({
-  origin: FRONTEND_URL,
-  credentials: true
-}));
-app.use(express.json());
+
+
+// app.use(cors({
+//   origin: FRONTEND_URL,
+//   credentials: true
+// }));
+
+
+// app.use(express.json());
+
+
+// app.use("/api/auth", authRoutes);
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
@@ -25,9 +30,10 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB Connected Successfully from Backend!'))
 .catch(err => {
     console.error('Database connection error:', err);
+   
 });
 
-
+// העירי זמנית את Socket.IO
 /*
 const io = socketIo(server, {
   cors: {
@@ -38,23 +44,19 @@ const io = socketIo(server, {
 
 io.on("connection", (socket) => {
   console.log("New client connected:", socket.id);
-
   socket.on("send-song", (data) => {
     console.log("Song sent:", data);
     io.emit("receive-song", data);
   });
-
   socket.on("disconnect", () => {
     console.log("Client disconnected:", socket.id);
   });
 });
 */
 
-
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+  res.send('Minimal Backend is running!');
 });
-
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
