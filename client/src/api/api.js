72d79ac08ad40f1d3c548f135b49391058fac8a4
ophir segal <ophirsegal@ophirsMacBook.local>
2025-06-13 17:29:03 +0300

    fix signup

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:4000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://jamoveo-production-46cf.up.railway.app/api",
 });
 
 export default api;
